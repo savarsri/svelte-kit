c2cabe42ee272ab2b0ea55d80ef1199f670c6902
Savar Srivastava <savarsri@gmail.com>
2023-04-08 23:50:43 +0530

    Dynamic routes

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Product list</h1>
+<ul>
+    <a href="/products/1">
+        <li>Product 1</li>
+    </a>
+    <a href="/products/2">
+        <li>Product 2</li>
+    </a>
+    <a href="/products/3">
+        <li>Product 3</li>
+    </a>
+</ul>
